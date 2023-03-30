910dea7a47e7739652ee4bd2697452707c50b554
Gottiking101 <121651046+Gottiking101@users.noreply.github.com>
2023-03-30 10:02:08 -0500

    Delete backend.tf

--- .github/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    encrypt = true    
-    bucket = "terrabucket101"
-    dynamodb_table = "terraform-state-lock-dynamo"
-    key    = "terraform.tfstate"
-    region = "us-east-2"
-  }
-}
