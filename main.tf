05ee421ee2b2b8c963051ce143703807b179c35b
Gottiking101 <121651046+Gottiking101@users.noreply.github.com>
2023-04-04 17:32:39 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "ansible_instance_1" {
   ami           = "ami-0a695f0d95cefc163"
   instance_type = "t2.micro"
-  count         = 2
+  count         = 3
   
   tags = {
     Name = "ansible-instance-${count.index + 1}"
