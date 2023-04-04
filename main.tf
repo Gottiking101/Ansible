670c47bce05e2a6a28ef3d14cb6e143cabc9bf34
Gottiking101 <121651046+Gottiking101@users.noreply.github.com>
2023-04-04 17:30:11 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "ansible_instance_1" {
+  ami           = "ami-0a695f0d95cefc163"
+  instance_type = "t2.micro"
+  count         = 2
+  
+  tags = {
+    Name = "ansible-instance-${count.index + 1}"
+  }
+}
+
